e54cc9066df77a20f82f7ad7038391bb60306544
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2020-09-22 13:23:11 +0200

    Finished chapter 7

---
+++ angular-book-tutorial-ch7/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <login></login>
+  `,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-book-tutorial-ch7';
+}
---
+++ angular-book-tutorial-ch7/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {LoginComponent} from './login.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {LoginService} from "./login.service";
+
+@NgModule({
+  declarations: [
+    AppComponent, LoginComponent
+  ],
+  imports: [
+    BrowserModule, ReactiveFormsModule
+  ],
+  providers: [LoginService],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
