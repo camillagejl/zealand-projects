33788726f265a250f646d614c673d4159235d5f5
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2020-09-23 11:40:57 +0200

    Finished chapter 8

---
+++ angular-book-tutorial-ch8/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {filter, debounceTime, distinctUntilChanged} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  template: `
+      <input
+          class="form-control"
+          type="search"
+          [formControl]="searchControl"
+      >
+
+      <div *ngIf="searchControl.touched">I'm touched!</div>
+  `
+})
+
+export class AppComponent {
+  searchControl = new FormControl();
+
+  constructor() {
+    this.searchControl.valueChanges
+      .pipe(filter(text => text.length >= 3), debounceTime(400), distinctUntilChanged())
+      .subscribe(value => {
+        console.log(value);
+      });
+  }
+}
+
