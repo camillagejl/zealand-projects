c95529a843e9f61aaa17e7b38488604c9f2ce301
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2020-09-23 21:23:21 +0200

    Finished chapter 10 (in chapter 9 project)

--- angular-book-tutorial-ch9/src/app/app.module.ts
+++ angular-book-tutorial-ch9/src/app/app.module.ts
@@ -4,15 +4,24 @@ import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import {HomeComponent} from './home.component';
+import {NotFoundComponent} from './notfound.component';
+import {GitHubComponent} from './github.component';
+import {routing} from './app.routing';
+import {GitHubUserComponent} from './githubuser.component';
+import {AuthGuard} from './auth-guard.service';
+import {LoginService} from './login.service';
+import {LoginComponent} from './login.component';
+import {PreventUnsavedChangesGuard} from './prevent-unsaved-changes-guard.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent, NotFoundComponent, GitHubComponent, GitHubUserComponent, LoginComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, ReactiveFormsModule
+    BrowserModule, HttpClientModule, ReactiveFormsModule, routing
   ],
-  providers: [],
+  providers: [AuthGuard, LoginService, PreventUnsavedChangesGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-book-tutorial-ch9/src/app/github.component.ts
+++ angular-book-tutorial-ch9/src/app/github.component.ts
@@ -4,25 +4,34 @@ import {debounceTime, distinctUntilChanged, filter} from 'rxjs/operators';
 import {FormControl} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
+  selector: 'github',
   template: `
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                      <a class="nav-link" routerLink="">Home <span class="sr-only">Home</span></a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" routerLink="GitHub">GitHub</a>
-                  </li>
-              </ul>
-          </div>
-      </nav>
-      <router-outlet></router-outlet>
+       
+    <h3>GitHub User Results</h3>
+    <input
+        class="form-control"
+        type="search"
+        [formControl]="searchControl"
+    >
+
+    <div *ngIf="isLoading">
+          <i class="fa fa-spinner fa-spin fa-3x"></i>
+      </div>
+    <div *ngFor="let user of users" class="media">
+        <div class="media">
+            <a [routerLink]="['user', user.login, user.score]">
+            <img src="{{ user.avatar_url }}" class="mr-3" alt="..." width="64" height="64">
+            </a>
+            <div class="media-body">
+                <h5 class="mt-0">{{ user.login }}</h5>
+                Score: {{ user.score }}
+            </div>
+        </div>
+    </div>
   `,
   providers: [GitHubService]
 })
-export class AppComponent {
+export class GitHubComponent {
 
   searchControl = new FormControl();
   isLoading = false;
